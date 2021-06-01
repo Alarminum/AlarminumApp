d64ff495aec9b83bed378c860e966fd9fa45bb1b
JaeHeyK <jaeheyk@gmail.com>
2021-06-01 23:24:13 +0900

    [Data] TimerListViewModel timer 관련 함수 추가

--- app/src/main/java/com/alarminum/alarminumapp/viewmodel/TimerListViewModel.java
+++ app/src/main/java/com/alarminum/alarminumapp/viewmodel/TimerListViewModel.java
@@ -1,4 +1,29 @@
 package com.alarminum.alarminumapp.viewmodel;
 
-public class TimerListViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.alarminum.alarminumapp.database.TimerEntity;
+import com.alarminum.alarminumapp.repository.AlarmRepository;
+
+import java.util.List;
+
+public class TimerListViewModel extends AndroidViewModel {
+    private final AlarmRepository alarmRepo;
+
+    private final LiveData<List<TimerEntity>> allTimers;
+
+    public TimerListViewModel (Application application) {
+        super(application);
+        alarmRepo = new AlarmRepository(application);
+        allTimers = alarmRepo.getAllTimers();
+    }
+
+    public LiveData<List<TimerEntity>> getAllTimers() { return allTimers; }
+
+    public void insert(TimerEntity timer) {
+        alarmRepo.insert(timer);
+    }
 }
