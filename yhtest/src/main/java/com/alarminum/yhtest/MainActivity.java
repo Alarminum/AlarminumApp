746313a1dfa660c0f33b8a8ee2fb0b901edc67c4
YongHwiGwon <u_uhh08@naver.com>
2021-06-19 00:00:21 +0900

    다른거 만지다가 고장났어서 미리 올려둡니당

--- yhtest/src/main/java/com/alarminum/yhtest/MainActivity.java
+++ yhtest/src/main/java/com/alarminum/yhtest/MainActivity.java
@@ -12,9 +12,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayAdapter<CharSequence> adspin1, adspin2;
+    ArrayAdapter<CharSequence> adspin1, adspin2, adspin3;
     String choice_do="";
     String choice_se="";
+    String choice_des="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -22,21 +23,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Spinner spin1 = (Spinner)findViewById(R.id.spinner);
         final Spinner spin2 = (Spinner)findViewById(R.id.spinner2);
+        final Spinner spin3 = (Spinner)findViewById(R.id.spinner3);
         Button btn_refresh = (Button)findViewById(R.id.btn_refresh);
 
-        adspin1 = ArrayAdapter.createFromResource(this, R.array.spinner_do, android.R.layout.simple_spinner_dropdown_item);
-        adspin1.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        adspin1 = ArrayAdapter.createFromResource(this, R.array.spinner_line, android.R.layout.simple_spinner_dropdown_item);
+        adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(adspin1);
 
 
         spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
-                if(adspin1.getItem(i).equals("서울")){
-                    choice_do = "서울";
-                    adspin2 = ArrayAdapter.createFromResource(MainActivity.this, R.array.spinner_do_seoul, android.R.layout.simple_spinner_dropdown_item);
+                if(adspin1.getItem(i).equals("1호선")){
+                    choice_do = "1호선";
+                    adspin2 = ArrayAdapter.createFromResource(MainActivity.this, R.array.spinner_start, android.R.layout.simple_spinner_dropdown_item);
+                    adspin3 = ArrayAdapter.createFromResource(MainActivity.this, R.array.spinner_start, android.R.layout.simple_spinner_dropdown_item);
 
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    adspin3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spin2.setAdapter(adspin2);
+                    spin3.setAdapter(adspin3);
                     spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
                         public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
@@ -47,16 +53,41 @@ public class MainActivity extends AppCompatActivity {
                         public void onNothingSelected(AdapterView<?> parent) {
                         }
                     });
-                }else if (adspin1.getItem(i).equals("인천")){
-                    choice_do = "인천";
-                    adspin2 = ArrayAdapter.createFromResource(MainActivity.this, R.array.spinner_do_incheon, android.R.layout.simple_spinner_dropdown_item);
+                    spin3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_des = adspin3.getItem(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                }else if (adspin1.getItem(i).equals("2호선")){
+                    choice_do = "2호선";
+                    adspin2 = ArrayAdapter.createFromResource(MainActivity.this, R.array.spinner_destination, android.R.layout.simple_spinner_dropdown_item);
+                    adspin3 = ArrayAdapter.createFromResource(MainActivity.this, R.array.spinner_destination, android.R.layout.simple_spinner_dropdown_item);
 
                     adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    adspin3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spin2.setAdapter(adspin2);
+                    spin3.setAdapter(adspin3);
                     spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
                         public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
                             choice_se = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                    spin3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            choice_se = adspin3.getItem(position).toString();
                         }
 
                         @Override
@@ -75,8 +106,10 @@ public class MainActivity extends AppCompatActivity {
         btn_refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, choice_do+"="+choice_se, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, choice_do+"="+choice_se + " 에서 " + choice_des, Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+
 }
